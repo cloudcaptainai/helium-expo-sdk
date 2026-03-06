24cb9381c051a78b0c04e0f396cd5be223e18d90
Kyle Gorlick <kyle@kylegorlick.com>
2026-03-05 21:11:42 -0800

    expo-paywall-sdk -> expo-helium

--- example/metro.config.js
+++ example/metro.config.js
@@ -19,7 +19,7 @@ config.resolver.nodeModulesPaths = [
 ];
 
 config.resolver.extraNodeModules = {
-  'expo-paywall-sdk': '..',
+  'expo-helium': '..',
 };
 
 config.watchFolders = [path.resolve(__dirname, '..')];
